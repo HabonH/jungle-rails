07e438759c6ca662ee1a2c00569591b9523d6d4e
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-06 14:44:11 -0500

    Add product and categories count to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
+# Display a count of how many products are in the database
+# Display a count of how many categories are in the database
 class Admin::DashboardController < ApplicationController
   def show
+    @product_total = Product.count
+    @category_total = Category.count
+    
   end
 end
