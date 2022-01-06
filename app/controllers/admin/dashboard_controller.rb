ffd08774cfa4505e7620f28b5fb34f7ca7f4a595
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-06 16:19:24 -0500

    Add About Us to website

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,3 @@
-# Display a count of how many products are in the database
-# Display a count of how many categories are in the database
 class Admin::DashboardController < ApplicationController
   def show
     @product_total = Product.count
